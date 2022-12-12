b55a453dd4da2989e4738b74e1ac20847f360c4c
mehedi.hasan <mehedisarker379@gmail.com>
2022-12-12 17:01:48 +0600

    factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+        ],
+        [
+            'name' => 'mehedi',
+            'email' => 'mehedi@gmail.com',
+        ],
+        [
+            'name' => 'siam',
+            'email' => 'siam@gmail.com',
+        ],
+    );
     }
 }
