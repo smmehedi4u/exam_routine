ec3b2a9e47904b3174c52910a12497d67ce45127
Amsiam <amsiam990@gmail.com>
2022-12-12 17:01:00 +0600

    Stisla admin panel config

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,63 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.guest')
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('title', 'Login')
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
 
-            <!-- Email Address -->
-            <div>
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@section('main')
+<div class="card card-primary">
+    <div class="card-header">
+        <h4>Login</h4>
+    </div>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <div class="card-body">
+        <form method="POST" action="">
+            {{ csrf_field() }}
+
+
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input id="email" type="text" value="{{ old('email') }}" class="form-control"
+                    name="email" tabindex="1" required="" autofocus="">
+                <div class="invalid-feedback">
+                    Please fill in your email
+                </div>
             </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <div class="form-group">
+                <div class="d-block">
+                    <label for="password" class="control-label">Password</label>
+                </div>
+                <input id="password" type="password" class="form-control" name="password" tabindex="2"
+                    required="">
+                <div class="invalid-feedback">
+                    please fill in your password
+                </div>
             </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
+            <div class="form-group">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" name="remember" class="custom-control-input" tabindex="3"
+                        id="remember-me">
+                    <label class="custom-control-label" for="remember-me">Remember Me</label>
+                </div>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-primary-button>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                    Login
+                </button>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+</div>
+@endsection
+
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,95 @@
+@php
+
+
+$links = [
+    [
+        'href' => 'dashboard',
+        'text' => 'Dashboard',
+        'section_text' => 'Dashboard',
+        'is_multi' => false,
+        'has_permission' => true,
+    ],
+    [
+        'href' => [
+            [
+                'section_text' => 'Department',
+                'section_list' => [
+                    ['href' => 'department.create', 'text' => 'Add', 'has_permission' => true],
+                    ['href' => 'department.index', 'text' => 'List', 'has_permission' => true],
+                ],
+            ],
+        ],
+        'text' => 'Department',
+        'is_multi' => true,
+        'has_permission' => true,
+],
+    [
+        'href' => [
+            [
+                'section_text' => 'Batch',
+                'section_list' => [
+                    ['href' => 'batch.create', 'text' => 'Add', 'has_permission' => true],
+                    ['href' => 'batch.index', 'text' => 'List', 'has_permission' => true],
+                ],
+            ],
+        ],
+        'text' => 'Batch',
+        'is_multi' => true,
+        'has_permission' => true,
+    ]
+];
+$navigation_links = json_decode(json_encode($links), false);
+@endphp
+
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="{{ route('dashboard') }}">Dashboard</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="{{ route('dashboard') }}">
+                <img class="d-inline-block" width="32px" height="30.61px" src="" alt="">
+            </a>
+        </div>
+        @foreach ($navigation_links as $link)
+            <ul class="sidebar-menu">
+                @if (!isset($link->has_permission) || $link->has_permission)
+                    <li class="menu-header">{{ $link->text }}</li>
+                    @if (!$link->is_multi)
+                        <li class="{{ Request::routeIs($link->href) ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route($link->href) }}"><i
+                                    class="fas fa-fire"></i><span>{{ $link->section_text }}</span></a>
+                        </li>
+                    @else
+                        @foreach ($link->href as $section)
+                            @php
+                                $routes = collect($section->section_list)
+                                    ->map(function ($child) {
+                                        return Request::routeIs($child->href);
+                                    })
+                                    ->toArray();
+
+                                $is_active = in_array(true, $routes);
+                            @endphp
+
+                            <li class="dropdown {{ $is_active ? 'active' : '' }}">
+                                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
+                                        class="fas fa-chart-bar"></i> <span>{{ $section->section_text }}</span></a>
+                                <ul class="dropdown-menu">
+                                    @foreach ($section->section_list as $child)
+                                        @if ($child->has_permission)
+                                            <li class="{{ Request::routeIs($child->href) ? 'active' : '' }}">
+                                                <a class="nav-link"
+                                                    href="{{ route($child->href) }}">{{ $child->text }}</a>
+                                            </li>
+                                        @endif
+                                    @endforeach
+                                </ul>
+                            </li>
+                        @endforeach
+                    @endif
+                @endif
+            </ul>
+        @endforeach
+    </aside>
+</div>
